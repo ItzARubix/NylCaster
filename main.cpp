1555bc00597362b34d4fba7f3fe892f84242309a
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2024-12-28 12:22:30 -0500

    Add deltaTime and make other actions reliant on it

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ int main() {
 	// Initialize camera position and angle
 	double camX = 50;
 	double camY = 50;
-	int camAngle = 0; // Measured from typical x-axis, and clockwise (rather than typical CCW)
+	double camAngle = 0; // Measured from typical x-axis, and clockwise (rather than typical CCW)
+
+	// Create the clock (this will be used for calculating deltaTime, which is the time between frames)
+	sf::Clock clock;
+	sf::Time deltaTime; // I must multiply everything by deltaTime. Otherwise, I am committing a crime
 
 	// Create the level layout
 	int levelLayout[5][5] = {
@@ -33,37 +37,37 @@ int main() {
 			window.close(); // ... close the window
 		}
 
+		// (DEBUG) print the deltaTime
+		// std::cout << "deltaTime is " << deltaTime.asSeconds() << "\n";
+
+		// Define deltaCamAngle, the amount that CamAngle should change each frame
+		double deltaCamAngle = 5*deltaTime.asSeconds();
 		// Change the camera orientation based on left/right input
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
-			camAngle-=1;
+			camAngle-=deltaCamAngle;
 			if(camAngle<0) {
 				camAngle+=360;
 			}
 			std::cout << "Camr angle is: " << camAngle << "\n";
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
-			camAngle+=1;
+			camAngle+=deltaCamAngle;
 			if(camAngle>360) {
 				camAngle-=360;
 			}
 			std::cout << "Camr angle is: " << camAngle << "\n";
 		}
 
-		// Change the camera position based on WASD
+		// Velocity magnitude
+		double camSpeed = 5*deltaTime.asSeconds();
+		// Change the camera position based on WASD. Or, I guess, just W in this case. 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-			camX += cos(toRad(camAngle));
-			camY += sin(toRad(camAngle));
+			camX += cos(toRad(camAngle))*camSpeed;
+			camY += sin(toRad(camAngle))*camSpeed;
 
 			std::cout << "X position is: " << camX << "\n";
 			std::cout << "Y position is: " << camY << "\n";
-
-			/*
-			camAngle+=1;
-			if(camAngle>360) {
-				camAngle-=360;
-			}
-			std::cout << camAngle << "\n";
-			*/
+			
 		}
 
 		// Each frame, assuming the window wasn't closed, run these lines: 
@@ -73,6 +77,8 @@ int main() {
 		window.draw(shape); 
 		
 		window.display(); // 3. Draw the framebuffer
+
+		deltaTime = clock.restart(); // 4. Save the deltaTime and restart the clock
 	}
 	return 0; // Return a normal exit code
 }
