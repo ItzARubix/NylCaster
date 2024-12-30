c88534a6ee8d6e8697d622a78725a2fd5cf57bbc
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2024-12-30 16:24:25 -0500

    Complete refactor of player movement.

--- main.cpp
+++ main.cpp
@@ -2,137 +2,115 @@
 #include <iostream>
 #include <cmath>
 
-float calculateScreenX(sf::Angle theta) {
-	return 180.f + 180.f*std::sin((9.f*theta.asRadians())/(4.f));
-	//return ((180.f*std::sin((9.f*theta.asRadians()))/(4.f))) + 180.f;
-}
+// Let it be known that I thusly have no idea how classes work in C++ lol. 
 
-int main() {
-
-	// Initialize player position and angle
-	sf::Vector2 playerPos(50.0f, 50.0f);
-	sf::Vector2 playerVision(1.0f, 0.0f);
-	// sf::Angle camAngle = sf::degrees(0); // Measured from typical x-axis, and clockwise (rather than typical CCW)
-
-	// Create a temporary quad. In particular, these are just two points, a left and right
-	sf::Vector2 quadLeft(100.0f, 49.5f);
-	sf::Vector2 quadRight(100.0f, 50.5f);
-
-	// Create the clock (this will be used for calculating deltaTime, which is the time between frames)
-	sf::Clock clock;
-	sf::Time deltaTime; // I must multiply everything by deltaTime. Otherwise, I am committing a crime
-
-	// Create the level layout
-	/*
-	int levelLayout[5][5] = {
-	{1, 1, 1, 1, 1},
-	{1, 0, 1, 0, 1},
-	{1, 0, 0, 0, 1},
-	{1, 0, 0, 0, 1},
-	{1, 1, 1, 1, 1}
-	};*/
-	// Create the window
-	sf::RenderWindow window(sf::VideoMode({640, 360}), "FeedTheBeast");
-
-	while (window.isOpen()) { // Run as long as the window is open
-		while (const std::optional event = window.pollEvent()) { // Check all of the window's events...
-			if (event->is<sf::Event::Closed>()) // ... and if one of the events was to close the window...
-			window.close(); // ... close the window
-		}
-
-		// (DEBUG) print the deltaTime
-		// std::cout << "deltaTime is " << deltaTime.asSeconds() << "\n";
-
-		// Define deltaCamAngle, the amount that CamAngle should change each frame
-		sf::Angle deltaCamAngle = sf::degrees(15*deltaTime.asSeconds());
-		// Change the camera orientation based on left/right input
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
-			playerVision = playerVision.rotatedBy(-1.0*deltaCamAngle);
-			std::cout << "Camr angle is: " << playerVision.angle().asDegrees() << "\n";
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
-			playerVision = playerVision.rotatedBy(deltaCamAngle);
-			std::cout << "Camr angle is: " << playerVision.angle().asDegrees() << "\n";
-		}
-
-		// Velocity magnitude
-		double playerSpeed = 12*deltaTime.asSeconds();
-		// Change the camera position based on WASD. Or, I guess, just W in this case. 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-			playerPos.x += std::cos(playerVision.angle().asRadians())*playerSpeed;
-			playerPos.y += std::sin(playerVision.angle().asRadians())*playerSpeed;
-
-			std::cout << "X position is: " << playerPos.x << "\n";
-			std::cout << "Y position is: " << playerPos.y << "\n";
+class Player { // Player class might be moved to a new file at some point. This is just for organization's sake.
+	public:
+		sf::Vector2<float> position;
+		sf::Vector2<float> velocity;
+		sf::Vector2<float> vision;
+		Player() : position(0.f, 0.f), velocity(0.f, 0.f), vision(1.f, 0.f) {
 			
 		}
+		~Player() {
+			
+		}
+};
 
-		// Here, we'll place the logic to allow our sample quad to be rendered
-		// Step 1: Determine the displacement vectors between the player and the left/right edges of the quad.
-		sf::Vector2 displacementLeft = quadLeft-playerPos;
-		sf::Vector2 displacementRight = quadRight-playerPos;
-		// Step 2: Determine the angles between the playerVision and each displacement.
-		sf::Angle quadLeftAngle = playerVision.angleTo(displacementLeft);
-		sf::Angle quadRightAngle = playerVision.angleTo(displacementRight);
-		// Step 3: Save the apparent height of each edge of the quad based on the displacement.
-		// float apparentHeightLeft = 100/(abs(std::cos(quadLeftAngle.asRadians()))*displacementLeft.length());
-		// float apparentHeightRight = 100/(abs(std::cos(quadRightAngle.asRadians()))*displacementRight.length());
-		float apparentHeightLeft = 40/(displacementLeft.length());
-		float apparentHeightRight = 40/(displacementRight.length());
+
+int main() {
 	
-		// Step 4: Convert the view angle to a place on screen
-		sf::ConvexShape quadDrawn;
-		quadDrawn.setPointCount(4);
-		
-		float leftScreenX = calculateScreenX(quadLeftAngle);
-		float rightScreenX = calculateScreenX(quadRightAngle);
-		quadDrawn.setPoint(0, {leftScreenX, 180-0.5f*apparentHeightLeft});
-		quadDrawn.setPoint(1, {leftScreenX, 180+0.5f*apparentHeightLeft});
-		quadDrawn.setPoint(2, {rightScreenX, 180+0.5f*apparentHeightRight});
-		quadDrawn.setPoint(3, {rightScreenX, 180-0.5f*apparentHeightRight});
-		
-		/*
-		quadDrawn.setPoint(0, {20.f, 20.f});
-		quadDrawn.setPoint(1, {20.f, 340.f});
-		quadDrawn.setPoint(2, {620.f, 300.f});
-		quadDrawn.setPoint(3, {620.f, 60.f});
-		*/
-		quadDrawn.setFillColor(sf::Color::Green);
+	// Create a player.
+	Player player;
 
-		// Print debug information if Z is being pressed
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z)) {
-			std::cout << "DEBUG:\n";
-			std::cout << "X position is:      " << playerPos.x << "\n";
-			std::cout << "Y position is:      " << playerPos.y << "\n";
-			std::cout << "Camr angle is:      " << playerVision.angle().asDegrees() << "\n";
-			std::cout << "LeftDisplacementX:  " << displacementLeft.x << "\n";
-			std::cout << "LeftDisplacementY:  " << displacementLeft.y << "\n";
-			std::cout << "RightDisplacementX: " << displacementRight.x << "\n";
-			std::cout << "RightDisplacementY: " << displacementRight.y << "\n";
-			std::cout << "QuadLeftAngle:      " << quadLeftAngle.asDegrees() << "\n";
-			std::cout << "QuadRightAngle:     " << quadRightAngle.asDegrees() << "\n";
-			std::cout << "ApparHeightLeft:    " << apparentHeightLeft << "\n";
-			std::cout << "ApparHeightRight:   " << apparentHeightRight << "\n";
-			std::cout << "Quad corner screencoords:\n";
-			std::cout << "0. X: " << leftScreenX << " Y: " << 180-0.5f*apparentHeightLeft << "\n";
-			std::cout << "1. X: " << leftScreenX << " Y: " << 180+0.5f*apparentHeightLeft << "\n";
-			std::cout << "2. X: " << rightScreenX << " Y: " << 180+0.5f*apparentHeightRight << "\n";
-			std::cout << "3. X: " << rightScreenX << " Y: " << 180-0.5f*apparentHeightRight << "\n";
-			std::cout << calculateScreenX(sf::degrees(-40)) << "\n";
-			std::cout << calculateScreenX(sf::degrees(40)) << "\n";
+	// Create the clock and a time variable to store deltaTime.
+	sf::Clock clock;
+	sf::Time deltaTime;
+
+	// Create the window.
+	sf::RenderWindow window(sf::VideoMode({640,360}), "FeedTheBeast");
+
+	while (window.isOpen()) { // Primary execution loop. This will run for as long as the window is open. 
+
+		while (const std::optional event = window.pollEvent()) { // This while-loop condition will only fail if there are no events left in the window.pollEvent() queue. Thus, for each iteration of this loop, until it fails, it pops one event off the queue, and holds onto it in the event variable...
+
+			if (event->is<sf::Event::Closed>()) { // ... and if that event is sf::Event::Closed...
+				window.close(); // ... close the window.
+			}
+
+		} // If the program makes it out of this loop, that means we can start the actual logic of the frame.
+
+		// CAMERA LOGIC.
+		// 1. Define deltaCamAngle, the amount that CamAngle should change each frame.
+		sf::Angle deltaCamAngle = sf::degrees(15*deltaTime.asSeconds());
+		// 2. If the player presses Left, the vision vector should be rotated negatively by deltaCamAngle.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+			player.vision = player.vision.rotatedBy(-1.f*deltaCamAngle);
+		}
+		// 3. If the player presses Right, the vision vector should be rotated positively by deltaCamAngle.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+			player.vision = player.vision.rotatedBy(deltaCamAngle);
 		}
 
-		// The last step in the loop is to actually draw everything to the screen. 
-		// Each frame, assuming the window wasn't closed, run these lines: 
-		window.clear(); // 1. Clear the framebuffer
+		// VELOCITY LOGIC.
+		// 1. Define playerSpeed, a scalar that we'll use to ensure the magnitude of the player velocity remains consistent.
+		float playerSpeed = 12.f;
+		// 2. Create a temporary vector that encodes all the directional information about the player movement.
+		sf::Vector2 velocityDirection(0.f, 0.f);
+		// 3. Forward/backward direction is governed by player.vision.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			velocityDirection += player.vision;
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+			velocityDirection -= player.vision;
+		}
+		// 4. Strafing is governed by a vector orthogonal to player.vision, which we define and then use.
+		sf::Vector2 strafeVector = player.vision.rotatedBy(sf::degrees(90));
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+			velocityDirection += strafeVector;
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			velocityDirection -= strafeVector;
+		}
+		// 5. We want player.velocity to be in the direction of velocityDirection and with the magnitude of playerSpeed.
+		if(velocityDirection.lengthSquared()!=0) {
+			player.velocity = playerSpeed*velocityDirection.normalized();
+		} else {
+			player.velocity = velocityDirection;
+		}
 
-		// 2. Draw everything into the framebuffer that you must
-		window.draw(quadDrawn); 
-		
-		window.display(); // 3. Draw the framebuffer
+		// POSITION LOGIC.
+		// 1. Add the player's velocity to their position, scaled by deltaTime.
+		player.position += deltaTime.asSeconds()*player.velocity;
 
-		deltaTime = clock.restart(); // 4. Save the deltaTime and restart the clock
+		// (DEBUG) print all information about player if they press Z.
+		// This is done at the end, right before the draw step. 
+		// This is because we want the debug info to reflect changes made in the current frame.
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z)) {
+			std::cout << "DEBUG: \n";
+			std::cout << "Player position: \n";
+			std::cout << "X: " << player.position.x << " Y: " << player.position.y << "\n";
+			std::cout << "Player velocity: \n";
+			std::cout << "X: " << player.velocity.x << " Y: " << player.velocity.y << "\n";
+			std::cout << "Player vision: \n";
+			std::cout << "X: " << player.vision.x << " Y: " << player.vision.y << "\n";
+			std::cout << "Player angle (relative to positive X): " << player.vision.angle().asDegrees() << "\n";
+			std::cout << "deltaTime: " << deltaTime.asSeconds() << "\n";
+		}
+
+		// This is the "draw step". It's where we actually draw everything to the screen.
+		// 1. Clear the framebuffer.
+		window.clear();
+
+		// 2. Draw everything to the framebuffer that you must.
+		// window.draw(shape);
+
+		// 3. Draw the framebuffer.
+		window.display();
+
+		// 4. Save the deltaTime and restart the clock.
+		deltaTime = clock.restart();
 	}
-	return 0; // Return a normal exit code. Don't think this is necessary but it's ok.
-}
 
+	return 0;
+}
