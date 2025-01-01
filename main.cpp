b8c45f5c99af69e15873747b5b4d446e26f96051
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2025-01-01 13:01:01 -0500

    The worst code known to mankind

--- main.cpp
+++ main.cpp
@@ -17,11 +17,29 @@ class Player { // Player class might be moved to a new file at some point. This 
 		}
 };
 
+class Wall {
+	public:
+		sf::Vector2<float> left;
+		sf::Vector2<float> right;
+		Wall() : left(10.f, -5.f), right(10.f, 5.f) {
+			// This is temporary. Will edit this to give real coords to the wall later.
+		}
+		~Wall() {
+
+		}
+};
+
 
 int main() {
+
+	// It is helpful to imagine the window as existing in worldspace. I precalculated this on paper, but this number is dependent on the FOV, so I might have to change it later.
+	constexpr float camToScreenDistance = 180/std::tan(2*M_PI/9);
 	
 	// Create a player.
 	Player player;
+
+	// (TEMP) Create a wall. 
+	Wall wall;
 
 	// Create the clock and a time variable to store deltaTime.
 	sf::Clock clock;
@@ -82,6 +100,8 @@ int main() {
 		// POSITION LOGIC.
 		// 1. Add the player's velocity to their position, scaled by deltaTime.
 		player.position += deltaTime.asSeconds()*player.velocity;
+		// It should be noted that this is essentially just deltaX = velocity*deltaTime, 
+		// which you should know from physics class.
 
 		// (DEBUG) print all information about player if they press Z.
 		// This is done at the end, right before the draw step. 
@@ -96,6 +116,7 @@ int main() {
 			std::cout << "X: " << player.vision.x << " Y: " << player.vision.y << "\n";
 			std::cout << "Player angle (relative to positive X): " << player.vision.angle().asDegrees() << "\n";
 			std::cout << "deltaTime: " << deltaTime.asSeconds() << "\n";
+			std::cout << "camToScreenDistance: " << camToScreenDistance << "\n";
 		}
 
 		// This is the "draw step". It's where we actually draw everything to the screen.
@@ -103,7 +124,39 @@ int main() {
 		window.clear();
 
 		// 2. Draw everything to the framebuffer that you must.
-		// window.draw(shape);
+		// RAYCASTING LOGIC.
+		// This takes place in a for-loop, where each iteration represents one column of pixels on the window. 
+		// In particular, as you shall see, the for-loop iterates from i=-180 to i=180, and the actual column being drawn to is i+180 (because the range of columns on screen is 0 to 360).
+		// We also have to calculate, based on the current iteration, what the angle that the ray should be cast at. Note that this is NOT a linear scale, and involves a teeny bit of trig.
+		for(int i=-180; i<=180; i++) {
+			sf::Angle rayDirectionAngle = sf::radians(std::atan(i/camToScreenDistance));
+			sf::Vector2 ray = 100.f*player.vision.rotatedBy(rayDirectionAngle);
+			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X)) {
+				std::cout << "For iteration " << i << " the rayDirectionAngle is " << rayDirectionAngle.asDegrees() << "\n";
+			}
+			
+			// IT'S TIME FOR THE WORST VARIABLE NAMES KNOWN TO MANKIND YAYYYY
+			// For the sake of not obfuscating what I'm doing here, https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment
+			// Godspeed
+			float x_1 = player.position.x;
+			float y_1 = player.position.y;
+			float x_2 = x_1 + ray.x;
+			float y_2 = y_1 + ray.y;
+			float x_3 = wall.left.x;
+			float y_3 = wall.left.y;
+			float x_4 = wall.right.x;
+			float y_4 = wall.right.y;
+			float t_numerator = ((x_1-x_3)*(y_3-y_4)) - ((y_1-y_3)*(x_3-x_4));
+			float t_denominator = ((x_1-x_2)*(y_3-y_4)) - ((y_1-y_2)*(x_3-x_4));
+			float s_numerator = ((x_1-x_2)*(y_1-y_3)) - ((y_1-y_2)*(x_1-x_3));
+			float s_denominator = ((x_1-x_2)*(y_3-y_4)) - ((y_1-y_2)*(x_3-x_4));
+			// The names are only slightly misleading:
+			// t is equal to t_numerator/t_denominator,
+			// but s is equal to the negative of s_numerator/s_denominator.
+			// There's no good reason to include the negative sign in the numerator over the denominator or vice versa,
+			// so I didn't bother. When I define s I'll just include the negative sign there.
+			if()
+		}
 
 		// 3. Draw the framebuffer.
 		window.display();
