257aa4b03f77e99233bd1236d90e80d30f93152b
ItzARubix <83780010+ItzARubix@users.noreply.github.com>
2025-01-02 13:53:06 -0500

    Refactored to allow for multiple walls

--- main.cpp
+++ main.cpp
@@ -21,13 +21,63 @@ class Wall {
 	public:
 		sf::Vector2<float> left;
 		sf::Vector2<float> right;
-		Wall() : left(10.f, -0.5f), right(10.f, 0.5f) {
-			// This is temporary. Will edit this to give real coords to the wall later.
+		Wall(float x_1, float y_1, float x_2, float y_2) {
+			left.x = x_1;
+			left.y = y_1;
+			right.x = x_2;
+			right.y = y_2;
 		}
 		~Wall() {
 
 		}
 };
+
+
+void rayCastingStep(sf::RenderWindow& window, Player player, std::vector<Wall> wallList) {
+	constexpr float camToScreenDistance = 320/std::tan(2*M_PI/9);
+	for(int i=-320; i<=320; i++) {
+		sf::Angle rayDirectionAngle = sf::radians(std::atan(i/camToScreenDistance));
+		sf::Vector2 ray = 100.f*player.vision.rotatedBy(rayDirectionAngle);
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::X)) {
+			std::cout << "For iteration " << i << " the rayDirectionAngle is " << rayDirectionAngle.asDegrees() << "\n";
+		}
+		
+		// IT'S TIME FOR THE WORST VARIABLE NAMES KNOWN TO MANKIND YAYYYY
+		// For the sake of not obfuscating what I'm doing here, https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line_segment
+		// Godspeed
+		sf::Vector2 currentShortestDisplacement(1000.f, 0.f);
+		for (Wall wall : wallList) {
+			float x_1 = player.position.x;
+			float y_1 = player.position.y;
+			float x_2 = x_1 + ray.x;
+			float y_2 = y_1 + ray.y;
+			float x_3 = wall.left.x;
+			float y_3 = wall.left.y;
+			float x_4 = wall.right.x;
+			float y_4 = wall.right.y;
+			float t_numerator = ((x_1-x_3)*(y_3-y_4)) - ((y_1-y_3)*(x_3-x_4));
+			float t_denominator = ((x_1-x_2)*(y_3-y_4)) - ((y_1-y_2)*(x_3-x_4));
+			float s_numerator = -1*(((x_1-x_2)*(y_1-y_3)) - ((y_1-y_2)*(x_1-x_3)));
+			float s_denominator = ((x_1-x_2)*(y_3-y_4)) - ((y_1-y_2)*(x_3-x_4));
+			if( (std::abs(t_numerator) < std::abs(t_denominator)) && (t_numerator*t_denominator > 0) && (std::abs(s_numerator) < std::abs(s_denominator)) && (s_numerator*s_denominator > 0) ) {
+				float t = t_numerator/t_denominator;
+				sf::Vector2 intersectionPoint(x_1 + t*(x_2-x_1), y_1 + t*(y_2-y_1));
+				sf::Vector2 displacementVector = intersectionPoint-player.position;
+				if( displacementVector.lengthSquared() < currentShortestDisplacement.lengthSquared() ) {
+					currentShortestDisplacement = displacementVector;
+				}
+			}
+		}
+		float apparentHeight = 360/(std::abs(std::cos(rayDirectionAngle.asRadians()))*currentShortestDisplacement.length());
+		float screenColumn = i+320.f;
+		std::array line = 
+		{
+			sf::Vertex{sf::Vector2f(screenColumn, 180-(apparentHeight/2))},
+			sf::Vertex{sf::Vector2f(screenColumn, 180+(apparentHeight/2))}
+		};
+		window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
+	}
+}
 
 
 int main() {
@@ -39,7 +89,11 @@ int main() {
 	Player player;
 
 	// (TEMP) Create a wall. 
-	Wall wall;
+	Wall wallClose(10.f, -5.f, 10.f, 5.f);
+	Wall wallFar(20.f, -10.f, 20.f, 10.f);
+	std::vector<Wall> level;
+	level.push_back(wallClose);
+	level.push_back(wallFar);
 
 	// Create the clock and a time variable to store deltaTime.
 	sf::Clock clock;
@@ -60,7 +114,7 @@ int main() {
 
 		// CAMERA LOGIC.
 		// 1. Define deltaCamAngle, the amount that CamAngle should change each frame.
-		sf::Angle deltaCamAngle = sf::degrees(15*deltaTime.asSeconds());
+		sf::Angle deltaCamAngle = sf::degrees(35*deltaTime.asSeconds());
 		// 2. If the player presses Left, the vision vector should be rotated negatively by deltaCamAngle.
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
 			player.vision = player.vision.rotatedBy(-1.f*deltaCamAngle);
@@ -125,6 +179,9 @@ int main() {
 
 		// 2. Draw everything to the framebuffer that you must.
 		// RAYCASTING LOGIC.
+		rayCastingStep(window, player, level);
+
+		/*
 		// This takes place in a for-loop, where each iteration represents one column of pixels on the window. 
 		// In particular, as you shall see, the for-loop iterates from i=-180 to i=180, and the actual column being drawn to is i+180 (because the range of columns on screen is 0 to 360).
 		// We also have to calculate, based on the current iteration, what the angle that the ray should be cast at. Note that this is NOT a linear scale, and involves a teeny bit of trig.
@@ -165,6 +222,7 @@ int main() {
 				window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
 			}
 		}
+		*/
 
 		// 3. Draw the framebuffer.
 		window.display();
